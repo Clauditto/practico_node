c0a1f3b90e97e98a3e1c315f69865fead848ca6e
= <=>
2023-06-30 00:17:13 -0400

    last version

--- index.js
+++ index.js
@@ -4,42 +4,30 @@ const { engine } = require('express-handlebars');
 
 const PORT = 3000;
 
-// DATOS DE ENTRADA
-
-const productos = [
-    {nombre: 'Banana', file: 'banana.png'},
-    {nombre: 'Cebolla', file: 'cebolla.png'},
-    {nombre: 'Lechuga', file: 'lechuga.png'},
-    {nombre: 'Papa', file: 'papas.png'},
-    {nombre: 'Pimenton', file: 'pimenton.png'},
-    {nombre: 'Tomate', file: 'tomate.png'}
-];
-
-function list_products() {
-    return productos;
-};
-
-// 
-
-
 // Motor de plantillas
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 // Middlewares
+app.use(express.static("./www"));
 app.use('/bootstrap/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/bootstrap/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-// Jquery
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
-
-
 
 // Rutas
 app.get("/", function (req, res) {
-    res.render("home");
+    res.render("home",{ 
+            usuario: 'Sixto Guerra', 
+            productos: [
+                {nombre: 'Banana', file: 'banana.png'},
+                {nombre: 'Cebolla', file: 'cebollas.png'},
+                {nombre: 'Lechuga', file: 'lechuga.png'},
+                {nombre: 'Papa', file: 'papas.png'},
+                {nombre: 'Pimenton', file: 'pimenton.png'},
+                {nombre: 'Tomate', file: 'tomate.png'}
+            ]
+        }
+    );
 });
 app.listen(PORT, () => console.log(`Iniciando en el puerto ${PORT}`));
-
-
-console.log(list_products())
